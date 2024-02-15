02a08f2d30843472bd4d79102ca63d4464917861
Anton <sychiov2015@yandex.ru>
2024-02-16 02:50:04 +0500

    #

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,6 +8,21 @@ const Form = () => {
 	const [subject, setSubject] = useState('physical')
 	const { tg } = useTelegram()
 
+	const onSendData = useCallback(() => {
+		const data = {
+			country,
+			street,
+			subject,
+		}
+		tg.sendData(JSON.stringify(data))
+	}, [])
+
+	useEffect(() => {
+		tg.WebApp.onEvent('mainButtonClicked', onSendData)
+		return () => {
+			tg.WebApp.offEvent('mainButtonClicked', onSendData)
+		}
+	}, [])
 	useEffect(() => {
 		tg.MainButton.setParams({
 			text: 'Отправить данные',
