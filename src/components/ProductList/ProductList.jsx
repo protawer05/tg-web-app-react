c5a05ce30569f65bbd663a08e99bf1542b6b7629
Anton <sychiov2015@yandex.ru>
2024-02-21 14:47:20 +0500

    #

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProductList.css'
-const ProductList = () => {
-	return <div>ProductList</div>
+import ProductItem from '../ProductItem/ProductItem'
+import { useTelegram } from '../../hooks/useTelegram'
+const products = [
+	{ id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые' },
+	{ id: '2', title: 'Джинсы2', price: 50000, description: 'Синего цвета, прямые' },
+	{ id: '3', title: 'Джинсы3', price: 50000, description: 'Синего цвета, прямые' },
+	{ id: '4', title: 'Джинсы4', price: 500000, description: 'Синего цвета(нет), прямые' },
+	{ id: '5', title: 'Джинсы5', price: 5000000, description: 'Синего цвета, прямые' },
+]
+
+const getTotalPrice = items => {
+	return items.reduce((acc, item) => {
+		return (acc += item.price)
+	}, 0)
+}
+
+const ProductList = ({ products }) => {
+	const [addedItems, setAddedItems] = useState([])
+	const { tg } = useTelegram()
+	const onAdd = product => {
+		const alreadyAdded = addedItems.find(item => item.id === product.id)
+		let newItems = []
+		if (alreadyAdded) {
+			newItems = addedItems.filter(item => item.id !== product.id)
+		} else {
+			newItems = [...addedItems, product]
+		}
+
+		setAddedItems(newItems)
+		if (newItems.length === 0) {
+			tg.MainButton.hide()
+		} else {
+			tg.MainButton.show()
+			tg.MainButton.setParams({
+				text: `Купить ${getTotalPrice(newItems)}`,
+			})
+		}
+	}
+	return (
+		<div className='list'>
+			{products.map(item => (
+				<ProductItem product={item} onAdd={onAdd} className={'item'} />
+			))}
+		</div>
+	)
 }
 
 export default ProductList
