26758b2f5fe39faf4600b2cf0f168ca1c16e25e2
Anton <sychiov2015@yandex.ru>
2024-03-19 20:33:18 +0500

    #

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,18 +1,25 @@
 import axios from 'axios'
 const URL = 'https://fly-deciding-ray.ngrok-free.app' // url backend
 export default function useAxios() {
+	const instance = axios.create({
+		baseURL: URL,
+		headers: {
+			'Content-Type': 'application/json',
+			'ngrok-skip-browser-warning': 'true',
+		},
+	})
 	const getProducts = async () => {
-		const { data } = await axios.get(`${URL}/products`)
+		const { data } = await instance.get(`/products`)
 		return data
 	}
 	const postProduct = async product => {
-		await axios.post(`${URL}/products`, product)
+		await instance.post(`/products`, product)
 	}
 	const deleteProduct = async productId => {
-		await axios.delete(`${URL}/products/${productId}`)
+		await instance.delete(`/products/${productId}`)
 	}
 	const postWebData = async data => {
-		await axios.post(`${URL}/web-data`, data)
+		await instance.post(`/web-data`, data)
 	}
 	return { getProducts, postProduct, deleteProduct, postWebData }
 }
